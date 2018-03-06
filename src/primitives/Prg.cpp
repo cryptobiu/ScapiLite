bfaa1a566c322a3cb8fdde7335174beae30e1f6d
Lior Koskas <liork.cryptobiu@gmail.com>
2018-03-06 13:25:26 +0200

    Create AES PRG

---
+++ src/primitives/Prg.cpp
@@ -0,0 +1,47 @@
+#include "Prg.h"
+
+#define MBEDTLS_CIPHER_MODE_CTR
+
+PrgFromAES::PrgFromAES()
+{
+    m_isKeySet = false;
+}
+
+
+SecretKey PrgFromAES::generateKey(int keySize)
+{
+    int randomData = open("/dev/urandom", O_RDONLY);
+    byte key[keySize];
+
+    if (randomData < 0)
+    {
+        cerr << "Cannot open /dev/urandom" << endl;
+    }
+    else
+    {
+
+        ssize_t result = read(randomData, key, sizeof key);
+        if (result < 0)
+        {
+            cerr << "Error while reading from /dev/urandom" << endl;
+        }
+    }
+    vector<byte> vc(key, key+keySize);
+    SecretKey sk(vc,"PrgAES");
+    m_isKeySet = true;
+    return sk;
+}
+
+void PrgFromAES::setKey(SecretKey & secretKey)
+{
+    if (!m_isKeySet)
+    {
+        AES_init_ctx(&m_ctx, (uint8_t*)&(secretKey.getEncoded()).at(0));
+        m_isKeySet = true;
+    }
+}
+
+void PrgFromAES::getPRGBytes(vector<byte> & outBytes, int outOffset, int outlen)
+{
+    AES_CTR_xcrypt_buffer(&m_ctx, (uint8_t*)outBytes.data(), outBytes.size());    
+}
